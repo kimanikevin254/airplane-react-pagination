f3ec6def9ca86e1307aff4f8b74aa49428fa21ce
kimanikevin254 <kimanikevin254@gmail.com>
2023-04-30 19:58:56 +0300

    added project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import { useEffect, useState } from "react"
 import Layout from "./components/Layout"
 import ProductRow from "./components/ProductRow"
+import { MdOutlineNavigateNext, MdOutlineNavigateBefore } from 'react-icons/md'
 
 export default function App(){
   const [products, setProducts] = useState([])
   
-  // new states will go here 
+  const [productsPerPage, setProductsPerPage] = useState(5)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [productsToDisplay, setProductsToDisplay] = useState([])
+  const [pageNumbers, setPageNumbers] = useState([])
 
-  // calculateProductsToDisplay function will go here
+const calculateProductsToDisplay = (itemsPerPage=productsPerPage, currentViewPage=1) => {
+  setProductsPerPage(Number(itemsPerPage))
+  setCurrentPage(Number(currentViewPage))
+  const indexOfLastProductOnCurrentPage = currentViewPage * Number(itemsPerPage)
+  const indexOfFirstProductOnCurrentPage = indexOfLastProductOnCurrentPage - Number(itemsPerPage)
+  setProductsToDisplay(products.slice(indexOfFirstProductOnCurrentPage, indexOfLastProductOnCurrentPage))
 
-  const fetchProducts = () => {
-    fetch('https://dummyjson.com/products?limit=100')
-    .then(res => res.json())
-    .then(({ products }) => {
+  // page numbers
+  const totalPages = Math.ceil(products.length / Number(itemsPerPage))
+  setPageNumbers(Array.from({ length: totalPages }, (_, i) => i + 1))
+}
+
+const fetchProducts = () => {
+  fetch('https://dummyjson.com/products?limit=100')
+  .then(res => res.json())
+  .then(({ products }) => {
       setProducts(products)
-    })
-  }
+      setProductsToDisplay(products.slice(0, 5))
+      const totalPages = Math.ceil(products.length / 5)
+      setPageNumbers(Array.from({ length: totalPages }, (_, i) => i + 1))
+  })
+}
 
   useEffect(() => {
     fetchProducts()
@@ -25,9 +42,24 @@ export default function App(){
       <div className='mb-8 flex items-center gap-12'>
         <h1 className="text-3xl font-bold">Products</h1>
 
-        {/* "products per page" dropdown will go here */}
+        <div className="border p-2 flex gap-3">
+          <h3>Products per page</h3>
+          <select className="px-2 py-1" value={productsPerPage} onChange={e => calculateProductsToDisplay(e.target.value, undefined)}>
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="20">20</option>
+          </select>
+      </div>
 
-        {/* next and previous buttons will go here */}
+        <div className="flex gap-3">
+            <button disabled={currentPage === 1} onClick={() => calculateProductsToDisplay(undefined, currentPage-1)} className="px-4 py-1 bg-gray-200 rounded disabled:bg-red-400">
+            <MdOutlineNavigateBefore />
+            </button>
+
+            <button disabled={currentPage === pageNumbers.length} onClick={() => calculateProductsToDisplay(undefined, currentPage+1)} className="px-4 py-1 bg-gray-200 rounded disabled:bg-red-400">
+            <MdOutlineNavigateNext />
+            </button>
+        </div>
       </div>
       
       <div className="flex flex-wrap gap-6 mb-32">
@@ -49,14 +81,20 @@ export default function App(){
 
           <tbody>
             {
-              products.length > 0 && products.map(product => (
-                <ProductRow product={product} />
-              ))
+                productsToDisplay.length > 0 && productsToDisplay.map(product => (
+                    <ProductRow key={product.id} product={product} />
+                ))
             }
           </tbody>
         </table>
 
-        {/* page numbers will go here */}
+        <div className="mt-8 w-fit mx-auto flex gap-2">
+            {
+                pageNumbers.map((num, i) => (
+                    <button key={i} onClick={() => calculateProductsToDisplay(undefined, num)} className={num === currentPage ? "px-4 py-1 bg-gray-500 text-white rounded" : "px-4 py-1 bg-gray-200 rounded"}>{num}</button>
+                ))
+            }
+        </div>
 
       </div>
     </Layout>
